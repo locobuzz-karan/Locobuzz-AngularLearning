93499457d9bdb8cf187ada481c653a8f5062fdaa
locobuzz-karan <karan.depale@locobuzz.com>
2025-05-23 17:34:31 +0530

    Day 2: TwoWayDataBinnding & Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
+import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-binding.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, BodyComponent,FooterComponent],
+  imports: [RouterOutlet, HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-binding.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:'TwoWayBinding' , component:TwoWayDataBindingComponent},
+     { path: 'About', component: AboutComponent },     
+];
