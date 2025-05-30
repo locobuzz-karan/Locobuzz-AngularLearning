b5721425480cdd437c55fe260684a96254446f77
locobuzz-karan <karan.depale@locobuzz.com>
2025-05-30 10:05:12 +0530

    ToDo App

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { AboutComponent } from './about/about.component';
 import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-binding.component';
 import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+import { ToDoAppComponent } from './to-do-app/to-do-app.component';
 
 
 export const routes: Routes = [
      {path:'TwoWayBinding' , component:TwoWayDataBindingComponent},
      {path: 'About', component: AboutComponent },  
      {path:'TemplateDrivenForm', component:TemplateDrivenFormComponent},
-     {path:'ReactiveForm',component:ReactiveFormsComponent} 
+     {path:'ReactiveForm',component:ReactiveFormsComponent},
+     {path:'ToDoApp' , component:ToDoAppComponent}
 ];
---
+++ src/app/to-do-app/to-do-app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-to-do-app',
+  imports: [FormsModule,CommonModule], 
+  templateUrl: './to-do-app.component.html',
+  styleUrl: './to-do-app.component.css'
+})
+export class ToDoAppComponent {
+   newTask: string = '';
+  tasks: string[] = [];
+
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.push(this.newTask.trim());
+      this.newTask = ''; // clear input
+    }
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+
+
+}
